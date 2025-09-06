6fe23a20f34d2f3e481b00153d13a81f4db257d1
Saurabh Kumar <saurabhkumar7253935873@gmail.com>
2025-09-06 22:15:33 +0530

    Add files via upload

---
+++ zoo park.py
@@ -0,0 +1,17 @@
+
+
+print(" Welcome to the zoo park")
+
+age = int(input("Enter your age"))
+
+if( age >=50):
+
+    print("you give 500 rupees") 
+
+elif( age  >=18):
+
+    print( " you pay 200 rupees")
+
+else: 
+
+    print("Zoo park is free for you ")
